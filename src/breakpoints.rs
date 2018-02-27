cbca552218b3a719ae9addf0534bab8c5a485ab5
Gaz Liddon Retina <gaz@gazaxian.com>
2018-02-27 07:26:11 +0000

    Changed Breakpoint interface

--- src/breakpoints.rs
+++ src/breakpoints.rs
@@ -1,51 +1,49 @@
-#[derive(Debug,PartialEq, PartialOrd)]
+#[derive(Clone, Debug,PartialEq, PartialOrd)]
 enum BreakpointTypes {
-    READ8,
-    WRITE8,
-    READ16,
-    WRITE16,
+    READ,
+    WRITE,
     EXEC,
 }
 
-#[derive(Debug, PartialEq, PartialOrd)]
-struct Bp {
+#[derive(Clone, Debug, PartialEq, PartialOrd)]
+pub struct BreakPoint {
     addr : u16,
     kind : BreakpointTypes,
 }
 
 struct Breakpoints {
-    break_points : Vec<Bp>,
+    break_points : Vec<BreakPoint>,
 }
 
 impl Breakpoints {
+
     pub fn new() -> Breakpoints {
         Breakpoints {
             break_points : vec![],
         }
     }
 
-    fn find (&mut self, addr : u16, kind : BreakpointTypes ) -> Option<usize> {
-        let this_bp = Bp { addr : addr, kind : kind };
+    fn find(&mut self, b : &BreakPoint) -> Option<usize> {
         let mut it = self.break_points.iter();
-        it.position(|bp| *bp == this_bp)
+        it.position(|bp| *bp == *b)
     }
 
+    fn add_breakpoint(&mut self, b : &BreakPoint ) {
+        if let Some(x) = self.find(b) {
+            self.clear_breakpoint(b)
+        }
 
-    fn add_breakpoint(&mut self, addr : u16, kind : BreakpointTypes ) {
-        let bp = Bp {addr: addr, kind : kind};
-        self.break_points.push(bp);
+        self.break_points.push(b.clone());
     }
 
-    fn clear_breakpoint(&mut self,  addr : u16, kind : BreakpointTypes ) {
-        let exists = self.find(addr, kind);
-
-        if  exists.is_some() {
-            self.break_points.remove( exists.unwrap() );
-        };
+    fn clear_breakpoint(&mut self,  b : &BreakPoint) {
+        if let Some(i) = self.find(b) {
+            self.break_points.remove(i);
+        }
     }
 
-    fn action(&mut self, addr : u16, kind : BreakpointTypes ) -> bool {
-        self.find(addr, kind).is_some()
+    fn clear_breakpoints_at_addr(&mut self, addr : u16) {
+        panic!("tbd")
     }
 
 }
